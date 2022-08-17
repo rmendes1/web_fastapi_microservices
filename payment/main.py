ad4ae67e540f49518fec2cd562dd7065c4192adb
joaormendes <joaorenatomendes@fontedeprecos.com.br>
2022-08-17 19:58:55 -0300

    Async function to create an order object created. It interacts with inventory through get requests

--- payment/main.py
+++ payment/main.py
@@ -48,3 +48,31 @@ class Order(HashModel):
 def get(pk: str):
     return Order.get(pk)
 
+
+@app.post('/orders')
+async def create(request: Request, background_tasks: BackgroundTasks):  # id, quantity
+    body = await request.json()
+
+    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+    product = req.json()
+
+    order = Order(
+            product_id=body['id'],
+            price=product['price'],
+            fee=0.2 * product['price'],
+            total=1.2 * product['price'],
+            quantity=body['quantity'],
+            status='pending'
+    )
+
+    order.save()
+
+    background_tasks.add_task(order_completed, order)
+
+    return order
+
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
