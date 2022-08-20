32628562afdcde923e43b2e0f064491200ecf52f
joaormendes <joaorenatomendes@fontedeprecos.com.br>
2022-08-20 15:13:14 -0300

    code from app removed to be put in components

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,60 +1,7 @@
-import logo from './logo.svg';
+import {Products} from "./components/Products";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Company name</a>
-          <div className="navbar-nav">
-            <div className="nav-item text-nowrap">
-              <a className="nav-link px-3" href="#">Sign out</a>
-            </div>
-          </div>
-      </header>
-
-      <div className="container-fluid">
-        <div className="row">
-          <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-            <div className="position-sticky pt-3 sidebar-sticky">
-              <ul className="nav flex-column">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
-                    <span data-feather="home" className="align-text-bottom"></span>
-                    Dashboard
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                  <td>text</td>
-                </tr>
-                </tbody>
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
-    </div>
-  );
+  return <Products/>;
 }
 
 export default App;
